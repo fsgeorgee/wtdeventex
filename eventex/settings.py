702aeba38e01cb466ee305cc0108dda7f0995dc9
George Elias Ferreira da Silva <fs.georgee@gmail.com>
2012-11-03 14:53:08 -0200

    Removing commented lines.

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,16 +17,6 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + PROJECT_DIR.child('database.db'))
-    '''
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-    '''
 }
 
 # Local time zone for this installation. Choices can be found here:
